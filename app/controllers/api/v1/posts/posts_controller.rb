98931887030feba50e7dda573bae2a88318b19fe
nan <nandangsopyan08@gmail.com>
2022-04-20 13:31:56 +0700

    adding create method

--- app/controllers/api/v1/posts/posts_controller.rb
+++ app/controllers/api/v1/posts/posts_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::Posts::PostsController < ApplicationController
         @posts = Post.all
         render json: @posts
     end
+
+    # POST /api/v1/posts
+    def create
+        @post = Post.create(posts_params)
+
+        if @post.valid?
+            render json: @post, status: :created
+        else
+            render json: @post.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def posts_params
+        params.require(:post).permit(:title, :content)
+    end
 end
