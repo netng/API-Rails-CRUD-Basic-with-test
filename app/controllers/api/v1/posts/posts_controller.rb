f823a92c63a55b09d6af539e89be343abaf2fe40
nan <nandangsopyan08@gmail.com>
2022-04-20 13:52:55 +0700

    adding destroy method

--- app/controllers/api/v1/posts/posts_controller.rb
+++ app/controllers/api/v1/posts/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Posts::PostsController < ApplicationController
+    before_action :set_post, only: [:destroy]
 
     # GET /api/v1/posts
     def index
@@ -17,8 +18,23 @@ class Api::V1::Posts::PostsController < ApplicationController
         end
     end
 
+    # DELETE /api/v1/posts/:id
+    def destroy
+        if @post.nil?
+            render status: :unprocessable_entity
+        else
+            @post.destroy
+            render status: :no_content
+        end
+    end
+
     private
+
     def posts_params
         params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+        @post = Post.find_by_id(params[:id])
     end
 end
